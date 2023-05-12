6e6f905d47b371dcbb8ba40154b9ac99f7654e89
olafrank <olafrnk@gmail.com>
2023-05-12 16:56:35 +0100

    CRUD implemented

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,58 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+
+// GET(READ)
+export const GET = async (request, { params }) => {
+    try {
+        await connectToDB();
+
+        const prompt = await Prompt.findById(params.id).populate('creator');
+
+        if (!prompt) return new Response('prompt not found', { status: 404 });
+
+        return new Response(JSON.stringify(prompt), { status: 200 })
+    } catch (error) {
+        return new Response(('Failed to fetch all prompts'), { status: 500 })
+
+    }
+}
+
+// PATCH(UPDATE)
+export const PATCH = async (request, { params }) => {
+    const { prompt, tag } = await request.json();
+
+    try {
+        await connectToDB();
+
+        const existingPrompt = await Prompt.findById(params.id);
+
+        if (!existingPrompt) return new Response('prompt not found', { status: 404 })
+
+        existingPrompt.prompt = prompt;
+        existingPrompt.tag = tag;
+
+        await existingPrompt.save();
+
+        return new Response(JSON.stringify(existingPrompt), { status: 200 })
+    } catch (error) {
+        return new Response(('Failed to update prompts'), { status: 500 })
+    }
+}
+
+// DELETE
+export const DELETE = async (request, { params }) => {
+try {
+    await connectToDB();
+
+    await Prompt.findByIdAndDelete(params.id);
+
+    return new Response('prompt deleted successfully',{status:200})
+
+    
+} catch (error) {
+    return new Response(('Failed to delete prompts'), { status: 500 })
+    
+    
+}
+}
--- models/prompt.js
+++ models/prompt.js
@@ -2,20 +2,21 @@ import { Schema, model, models } from "mongoose";
 
 
 const PromptSchema = new Schema({
-creator:{
-    type:Schema.Types.ObjectId,
-    ref:'User',
-},
-prompt:{
-    type:String,
-    required:[true,'prompt is required.']
-},
-tag:{
-    type:String,
-    required:[true,'Tag is required']
-}
+    creator: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        
+    },
+    prompt: {
+        type: String,
+        required: [true, 'prompt is required.']
+    },
+    tag: {
+        type: String,
+        required: [true, 'Tag is required']
+    }
 })
 
-const Prompt =  models.Prompt || model('Prompt',PromptSchema)
+const Prompt = models.Prompt || model('Prompt', PromptSchema)
 
 export default Prompt;
