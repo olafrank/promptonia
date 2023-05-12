1108c56db02cef34e9908f28afdf9a06143aafe7
olafrank <olafrnk@gmail.com>
2023-05-12 10:36:12 +0100

    database mongodb prompt

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,10 +2,10 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google'
 
 
-
-
 import User from "@models/user";
 import { connectToDB } from '@utils/database'
+
+connectToDB();
 
 const handler = NextAuth({
     providers: [
@@ -14,41 +14,49 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ],
-    async session({ session }) {
-        const sessionUser = await User.findOne({
-            email: session.user.email
-        })
-
-        session.user.id = sessionUser._id.toString();
-
-        return session
-    },
-
-    async siginIn({ profile }) {
-        try {
-            await connectToDB()
-            // check if a user already exist
-            const userExists = await User.findOne({
-                email: profile.email
+    callbacks:{
+        async session({ session }) {
+            const sessionUser = await User.findOne({
+                email: session.user.email
             });
-
-            // if not,create a new user
-            if (!userExists) {
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ", "").toLowerCase(),
-                    image: profile.picture
-                })
+    
+            if (sessionUser) {
+                session.user.id = sessionUser._id.toString();
             }
-
-            return true;
-
-        } catch (error) {
-            console.log(error)
-            return false
+    
+            return session;
+        },
+       
+    
+        async signIn({ profile }) {
+            try {
+                
+                // check if a user already exist
+                const userExists = await User.findOne({
+                    email: profile.email
+                });
+    
+                // if not,create a new user
+                if (!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture
+                    });
+                }
+    
+                return true;
+    
+            } catch (error) {
+                console.log(error);
+                return false;
+            }
+    
         }
 
     }
-})
+       
+    
+});
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
