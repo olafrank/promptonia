d68477fe66c8a5e39ca1df8269ba0e14b1d26e08
olafrank <olafrnk@gmail.com>
2023-05-13 04:53:42 +0100

    promptcard error fixed fully functional

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -23,7 +23,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       <div className='flex justify-between items-start gap-5'>
         <div className='flex-1 flex justify-start items-center gap-3 cursor-pointer'>
           <Image
-            src={post.creator?.image}
+            src={post.creator.image}
             alt='user_image'
             width={40}
             height={40}
@@ -31,7 +31,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
           />
 
           <div className='flex flex-col'>
-            <h3 className='font-satoshi font-semibold text-gray-900'>{post.creator?.username}</h3>
+            <h3 className='font-satoshi font-semibold text-gray-900'>{post.creator.username}</h3>
             <p className='font-inter text-sm text-gray-500'>{post.creator?.email}</p>
 
           </div>
